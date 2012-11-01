6176a536c7886c79a7b3b66021fb50acf2a67c74
Martin Kleppmann <martin@rapportive.com>
2012-11-01 16:54:10 +0000

    Split invoicing_generator gem into separate git repo

--- invoicing/lib/invoicing/price.rb
+++
@@ -1,9 +0,0 @@
-module Invoicing
-  module Price
-    module ActMethods
-      def acts_as_price(*args)
-        
-      end
-    end
-  end
-end
--- invoicing/test/models/test_subclass_in_another_file.rb
+++
@@ -1,3 +0,0 @@
-# Used by find_subclasses_test.rb
-class TestSubclassInAnotherFile < TestBaseclass
-end
